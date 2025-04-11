74b668d758591ad0c6a136fca9f678f894109b1e
DeveloperMan313 <053000voloshch@gmail.com>
2025-04-12 02:24:57 +0300

    Add profileApi

---
+++ src/api/profileApi.ts
@@ -0,0 +1,59 @@
+'use strict';
+
+import { API, APIresponse } from '../modules/api';
+
+/**
+ * Интерфейс для описания данных профиля
+ */
+interface Profile {
+    username: string;
+    email: string;
+    description: string;
+    balance: number;
+    role: number;
+}
+
+export class ProfileAPI {
+    /**
+     * Получить информацию профиля
+     * @returns {Promise<Profile>} - ответ API
+     */
+    static async getMyInfo(): Promise<Profile> {
+        const response = await API.fetch('/profile/my', {
+            method: 'GET',
+            headers: {},
+        });
+        const json = await response.json();
+        return json['body'];
+    }
+
+    /**
+     * Обновить информацию профиля
+     * @param {Profile} profile - информация профиля
+     * @returns {Promise<APIresponse>} - ответ API
+     */
+    static async updateMyInfo(profile: Profile): Promise<APIresponse> {
+        const response = await API.fetch('/profile/edit', {
+            method: 'PUT',
+            body: JSON.stringify(profile),
+        });
+        return response.json();
+    }
+
+    /**
+     * Загрузить аватар для профиля
+     * @param {File} file - файл
+     * @returns {Promise<APIresponse>} - ответ API
+     */
+    static async uploadAvatar(file: File): Promise<APIresponse> {
+        const formData = new FormData();
+        formData.append('avatar', file);
+
+        const response = await API.fetch(`/avatar/upload`, {
+            method: 'PUT',
+            headers: {},
+            body: formData,
+        });
+        return response.json();
+    }
+}
