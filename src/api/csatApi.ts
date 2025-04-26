e0cf265844f5301de96a372112ae5595bf774f7a
WonderBro <froloff1830@gmail.com>
2025-04-26 15:48:57 +0300

    Add Local storage logic

--- src/api/csatApi.ts
+++ src/api/csatApi.ts
@@ -14,16 +14,43 @@ interface CsatReview {
 
 export class CsatAPI {
     /**
-     * Получить вопрос по странице
+     * Получить вопрос по странице или null
      * @param {string} page_id - имя страницы
      * @returns {Promise<APIresponse>} - ответ API
      */
     static async getQuestion(page_id: string): Promise<APIresponse> {
+        const now = Date.now();
+    
+        const cacheDataStr = localStorage.getItem('csatShowCache');
+        let cacheData: {[key: string]: number} = {};
+    
+        if (cacheDataStr) {
+            cacheData = JSON.parse(cacheDataStr);
+        }
+    
+        const lastShownTime = cacheData[page_id];
+        const ONE_MINUTE = 60 * 1000;
+    
+        if (lastShownTime && (now - lastShownTime) < ONE_MINUTE) {
+            return {
+                service: null,
+                body: {
+                  success: false,
+                  message: 'Запрос для этого page_id был сделан недавно. Попробуйте позже.',
+                  data: null
+                }
+              } as APIresponse;
+        }
+    
+        cacheData[page_id] = now;
+        localStorage.setItem('csatShowCache', JSON.stringify(cacheData));
+    
         const response = await API.fetch(`/csat/show/${page_id}`, {
             method: 'GET',
         });
         return response.json();
     }
+    
 
     /**
      * Отправить отзыв CSAT
