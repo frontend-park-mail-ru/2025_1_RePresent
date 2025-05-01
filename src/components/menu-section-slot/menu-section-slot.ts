a37ee77006aedc1e81c05f31368f2a1da6bd644a
DeveloperMan313 <053000voloshch@gmail.com>
2025-05-01 21:41:01 +0300

    Add menu-section-slot

---
+++ src/components/menu-section-slot/menu-section-slot.ts
@@ -0,0 +1,106 @@
+'use strict';
+
+import './menu-section-slot.scss';
+
+import { dispatcher } from '../../modules/dispatcher';
+import { Component } from '../../modules/component';
+import { MenuList, MenuListProps } from '../menu-list/menu-list';
+import { MenuBannerEditor } from '../menu-banner-editor/menu-banner-editor';
+import { store } from '../../modules/store';
+
+/**
+ * Раздел меню слотов
+ */
+export class MenuSectionSlot extends Component {
+    private selectedMenuName: string = '';
+
+    /**
+     * Конструктор компонента
+     * @param {HTMLElement} parent - родительский узел компонента
+     */
+    constructor(parent: HTMLElement) {
+        super(parent, 'menu-section-slot/menu-section-slot', {});
+
+        dispatcher.on('menu-select', (selectedName: string) => {
+            this.selectedMenuName = selectedName;
+            this.renderMenu();
+        });
+
+        dispatcher.on('store-updated-hasSlots', () => {
+            this.render();
+        });
+
+        dispatcher.on('store-updated-selectedSlot', () => {
+            this.render();
+            store.update({ key: 'fileId', value: '' });
+            if (this.selectedMenuName == '') {
+                this.selectedMenuName = 'editor';
+            }
+            dispatcher.dispatch('menu-select', this.selectedMenuName);
+        });
+
+        dispatcher.on('slot-delete', () => {
+            dispatcher.dispatch('menu-select', '');
+        });
+    }
+
+    /**
+     * Отрисовка выбранного меню
+     */
+    private renderMenu() {
+        const hasSelectedSlot = store.get<boolean>('selectedSlot');
+        if (!hasSelectedSlot) {
+            return;
+        }
+
+        const menuContents = this.rootElement.getElementsByClassName('menu-contents')[0] as HTMLElement;
+        menuContents.innerHTML = '';
+
+        switch (this.selectedMenuName) {
+            case 'editor':
+                new MenuBannerEditor(menuContents).render();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Отрисовка сообщения при отсутствии выбранного слота
+     */
+    private renderEmptyState(): void {
+        const hasSlots = store.get<boolean>('hasSlots');
+        let emptyStateMsg;
+        if (hasSlots) {
+            emptyStateMsg = 'Чтобы начать работу, выберите слот';
+        }
+        if (!hasSlots) {
+            emptyStateMsg = 'Чтобы начать размещать рекламу, создайте слот';
+        }
+        this.rootElement.insertAdjacentHTML('beforeend', `<p class="empty-state-msg">${emptyStateMsg}</p>`);
+    }
+
+    /**
+     * Отрисовка
+     */
+    render(): void {
+        super.render();
+
+        const hasSelectedSlot = store.get<boolean>('selectedSlot');
+        if (!hasSelectedSlot) {
+            this.renderEmptyState();
+            return;
+        }
+
+        const props: MenuListProps = {
+            items: [
+                { label: 'Редактор', menuName: 'editor' },
+                // { label: 'Статистика', menuName: 'statistics' },
+            ],
+        };
+
+        new MenuList(this.rootElement).render(props);
+
+        this.rootElement.insertAdjacentHTML('beforeend', '<div class="menu-contents"></div>');
+    }
+}
