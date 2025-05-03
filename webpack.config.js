89117dc5b7e8902a7172e184354667bfa0a64dd3
DeveloperMan313 <053000voloshch@gmail.com>
2025-05-03 04:41:45 +0300

    RE-069: Add mobile layout to sign in/up pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path');
 
+const stringReplaceLoader = {
+    loader: 'string-replace-loader',
+    options: {
+        multiple: [
+            { search: 'ENV_API_ORIGIN', replace: 'http://re-target.ru' },
+            { search: 'ENV_API_VERSION', replace: 'v1' },
+            { search: 'ENV_MOBILE_MAX_WIDTH_PX', replace: '500', flags: 'g' },
+        ],
+    },
+};
+
 const webpackModule = {
     rules: [
         {
             test: /\.tsx?$/,
-            use: 'ts-loader',
+            use: ['ts-loader', stringReplaceLoader],
             exclude: /node_modules/,
         },
         {
             test: /\.s[ac]ss$/i,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
+            use: ['style-loader', 'css-loader', stringReplaceLoader, 'sass-loader'],
         },
-        {
-            loader: 'string-replace-loader',
-            options: {
-                multiple: [
-                    { search: 'ENV_API_ORIGIN', replace: 'http://re-target.ru' },
-                    { search: 'ENV_API_VERSION', replace: 'v1' },
-                ],
-            },
-        }
     ],
 };
 
-const resolve = {
+const resolveScripts = {
     extensions: ['.tsx', '.ts', '.js'],
 };
 
@@ -35,7 +37,7 @@ module.exports = [
         devtool: 'source-map',
         entry: './src/index.ts',
         module: webpackModule,
-        resolve,
+        resolve: resolveScripts,
         output: {
             filename: 'bundle.js',
             path: distPath,
@@ -46,7 +48,7 @@ module.exports = [
         devtool: 'source-map',
         entry: './src/indexCsat.ts',
         module: webpackModule,
-        resolve,
+        resolve: resolveScripts,
         output: {
             filename: 'bundleCsat.js',
             path: distPath,
