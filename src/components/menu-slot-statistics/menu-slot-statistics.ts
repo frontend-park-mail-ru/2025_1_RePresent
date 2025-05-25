aa0953cb881f2790181c438c2563a476fbc92399
DeveloperMan313 <053000voloshch@gmail.com>
2025-05-25 13:58:34 +0300

    Connect to API

--- src/components/menu-slot-statistics/menu-slot-statistics.ts
+++ src/components/menu-slot-statistics/menu-slot-statistics.ts
@@ -5,6 +5,9 @@ import './menu-slot-statistics.scss';
 import { Component } from '../../modules/component';
 import { InputSelect } from '../input-select/input-select';
 import { LineChart } from '../line-chart/line-chart';
+import { AdvAPI } from '../../api/advApi';
+import { store } from '../../modules/store';
+import { Slot } from '../../api/slotApi';
 
 /**
  * Меню ститистики слота
@@ -25,29 +28,41 @@ export class MenuSlotStatistics extends Component {
     /**
      * Отрисовка графика
      */
-    private renderPlot() {
-        // TODO: API call
-        const randomData = function* (count: number) {
-            for (let i = 1; i <= count; i++) {
-                yield [i, Math.random() * 100] as [number, number];
-            }
-        };
-
+    private async renderPlot() {
         const metric = this.selectMetric.getValue() as string;
         const days = +this.selectPeriod.getValue();
-        const metricToYLabel = {
-            'shows': 'показы',
-            'revenue': 'доход, руб.',
-            'avg-show-price': 'ср. показ, руб.',
-        } as Record<string, string>;
 
-        const data = [...randomData(days)];
+        const from = new Date();
+        from.setUTCDate(from.getUTCDate() - days);
+        const response = await AdvAPI.getStats(
+            {
+                from: from,
+                to: new Date(),
+                activity: metric as 'click' | 'shown',
+                slot: store.get<Slot>('selectedSlot').link,
+            }
+        );
+        if (response.service.error) {
+            alert('Не удалось получить статистику слота');
+            return;
+        }
+        const dataRaw = Object.entries(response.body) as [string, string][];
+
+        const data = dataRaw.map(e => [Date.parse(e[0]) / 1000, parseFloat(e[1])]) as [number, number][];
+        const dataXtoLabel = Object.fromEntries(data.map((e, i) => [e[0], dataRaw[i][0]]));
         const dataY = data.map(e => e[1]);
         const sum = Math.round(dataY.reduce((a, b) => a + b, 0));
         const avg = Math.round(sum / dataY.length);
 
         const aggregates = this.rootElement.querySelector('.aggregates') as HTMLElement;
         aggregates.innerHTML = `<p class="metric-avg">Среднее: ${avg}</p><p class="metric-sum">Сумма: ${sum}</p>`
+
+        const metricToYLabel = {
+            'shown': 'показы',
+            'click': 'клики',
+            'revenue': 'доход, руб.',
+            'avg-show-price': 'ср. показ, руб.',
+        } as Record<string, string>;
 
         this.chart.render({
             id: 'chart',
@@ -70,7 +85,7 @@ export class MenuSlotStatistics extends Component {
                 y: 10,
             },
             dataLabelMap: {
-                x: (n: number) => `${n}.05`,
+                x: (n: number) => dataXtoLabel[n],
             },
         });
     }
@@ -87,8 +102,12 @@ export class MenuSlotStatistics extends Component {
             label: 'Метрика',
             options: [
                 {
-                    value: 'shows',
+                    value: 'shown',
                     label: 'Показы',
+                },
+                {
+                    value: 'click',
+                    label: 'Клики',
                 },
                 {
                     value: 'revenue',
