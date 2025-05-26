bee7c26414cd40ccc4ee249ef9b3afe1059891a2
DeveloperMan313 <053000voloshch@gmail.com>
2025-05-26 11:50:43 +0300

    Update wallet options

--- src/components/wallet-options/wallet-options.ts
+++ src/components/wallet-options/wallet-options.ts
@@ -7,12 +7,15 @@ import { Button } from '../button/button';
 import { InputField } from '../input-field/input-field';
 import { topUpAmountGetError } from '../../modules/validation';
 import { PaymentAPI } from '../../api/paymentApi';
+import { Profile } from '../../api/profileApi';
+import { store } from '../../modules/store';
+import { reAlert } from '../../modules/re-alert';
 
 /**
  * Настройки кошелька
  */
 export class WalletOptions extends Component {
-    private topUpAmountInput: InputField;
+    private amountInput: InputField;
 
     /**
      * Конструктор компонента
@@ -39,11 +42,11 @@ export class WalletOptions extends Component {
      * Обработчик нажатия на кнопку пополнения
      */
     private async onTopUpClick(): Promise<void> {
-        if (!this.topUpAmountInput.validate()) {
+        if (!this.amountInput.validate()) {
             return;
         }
 
-        const topUpAmount = <string>this.topUpAmountInput.getValue();
+        const topUpAmount = <string>this.amountInput.getValue();
         const key = crypto.randomUUID();
         const response = await PaymentAPI.topUp({
             value: topUpAmount,
@@ -54,9 +57,25 @@ export class WalletOptions extends Component {
         });
 
         if (!response.confirmation_url) {
-            alert('Ошибка пополнения счета');
+            reAlert({
+                message: 'Ошибка пополнения счета',
+                type: 'error',
+                lifetimeS: '5',
+            });
+            return;
         }
         location.href = response.confirmation_url;
+    }
+
+    /**
+     * Обработчик нажатия на кнопку снятия
+     */
+    private async onWithdrawClick(): Promise<void> {
+        reAlert({
+            message: 'Эта функция еще не реализована :(',
+            type: 'error',
+            lifetimeS: '5',
+        });
     }
 
     /**
@@ -65,18 +84,27 @@ export class WalletOptions extends Component {
     public render(): void {
         super.render();
 
+        const role = store.get<Profile>('profile').role;
         const walletMain = this.rootElement.getElementsByClassName('wallet-main')[0] as HTMLElement;
-        const topUpButton = new Button(walletMain);
-        topUpButton.render({ label: 'Пополнить', type: 'primary', onClick: this.onTopUpClick.bind(this) });
 
-        this.topUpAmountInput = new InputField(this.rootElement, {
-            label: 'Сумма пополнения',
-            name: 'top-up-amount',
+        if (role == 1) {
+            const topUpButton = new Button(walletMain);
+            topUpButton.render({ label: 'Пополнить', type: 'primary', onClick: this.onTopUpClick.bind(this) });
+        }
+
+        if (role == 2) {
+            const withdrawButton = new Button(walletMain);
+            withdrawButton.render({ label: 'Снять', type: 'primary', onClick: this.onWithdrawClick.bind(this) });
+        }
+
+        this.amountInput = new InputField(this.rootElement, {
+            label: `Сумма ${(role == 1) ? 'пополнения' : 'снятия'}`,
+            name: 'amount',
             type: 'text',
             placeholder: 'Введите сумму',
             getError: topUpAmountGetError,
         });
-        this.topUpAmountInput.render();
+        this.amountInput.render();
 
         this.renderAmount();
     }
